ed48da5b4c14e982daaabf7a0bab25724a1a5979
Theresia Givani Naibaho <19241999@bsi.ac.id>
2026-04-30 16:43:16 +0700

    Menambahkan database

---
+++ app/Models/SchoolSetting.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SchoolSetting extends Model
+{
+    protected $fillable = [
+        'name',
+        'latitude',
+        'longitude',
+        'radius_meters',
+        'check_in_start_time',
+        'check_in_end_time',
+        'late_tolerance_minutes',
+        'check_out_start_time',
+        'check_out_end_time',
+    ];
+
+    protected $casts = [
+        'latitude' => 'float',
+        'longitude' => 'float',
+        'radius_meters' => 'integer',
+        'check_in_start_time' => 'string',
+        'check_in_end_time' => 'string',
+        'late_tolerance_minutes' => 'integer',
+        'check_out_start_time' => 'string',
+        'check_out_end_time' => 'string',
+    ];
+
+    public static function singleton(): self
+    {
+        return static::query()->firstOrCreate(
+            ['id' => 1],
+            [
+                'name' => config('app.name', 'Sekolah'),
+                'latitude' => -6.2000000,
+                'longitude' => 106.8166667,
+                'radius_meters' => 50,
+                'check_in_start_time' => '07:00:00',
+                'check_in_end_time' => '08:00:00',
+                'late_tolerance_minutes' => 15,
+                'check_out_start_time' => '15:00:00',
+                'check_out_end_time' => '17:00:00',
+            ]
+        );
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,46 +3,57 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use App\Concerns\HasTeams;
-use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Str;
-use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
-#[Fillable(['name', 'email', 'password', 'current_team_id'])]
-#[Hidden(['password', 'two_factor_secret', 'two_factor_recovery_codes', 'remember_token'])]
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
-    use HasFactory, HasTeams, Notifiable, TwoFactorAuthenticatable;
+    use HasApiTokens, HasFactory, Notifiable;
+    use HasRoles;
 
     /**
-     * Get the attributes that should be cast.
+     * The attributes that are mass assignable.
      *
-     * @return array<string, string>
+     * @var array<int, string>
      */
-    protected function casts(): array
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'whatsapp_number',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    public function studentProfile(): HasOne
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-            'two_factor_confirmed_at' => 'datetime',
-        ];
+        return $this->hasOne(StudentProfile::class);
     }
 
-    /**
-     * Get the user's initials
-     */
-    public function initials(): string
+    public function teacher(): HasOne
     {
-        return Str::of($this->name)
-            ->explode(' ')
-            ->take(2)
-            ->map(fn ($word) => Str::substr($word, 0, 1))
-            ->implode('');
+        return $this->hasOne(Teacher::class);
     }
 }
